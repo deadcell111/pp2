50309b3afa86fa4c420ccf3581c29eb5680af13c
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-19 17:11:34 +0500

    arrays and multiple values

--- hw.py
+++ hw.py
@@ -1,11 +1,5 @@
-
-"""type if variables and multi comment
-Text Type:	str
-Numeric Types:	int, float, complex
-Sequence Types:	list, tuple, range
-Mapping Type:	dict
-Set Types:	set, frozenset
-Boolean Type:	bool
-Binary Types:	bytes, bytearray, memoryview
-None Type:	NoneType
-"""
+fruits = ["apple", "banana", "cherry"]  
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
