bff55ac6af6f8c2c2d49061183a570b14327ecd4
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-19 17:13:38 +0500

    functions

--- hw.py
+++ hw.py
@@ -1,5 +1,9 @@
-fruits = ["apple", "banana", "cherry"]  
-x, y, z = fruits
-print(x)
-print(y)
-print(z)
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
