98eeb70a2a6480dbebf4fdf6a8f7c52bbb0b34a9
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-19 17:06:22 +0500

    type if variables and multi comment

--- hw.py
+++ hw.py
@@ -1,1 +1,11 @@
-print("Hello, World!")
+
+"""type if variables and multi comment
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""
