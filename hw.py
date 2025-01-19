1e747c7abc6a99395d51fa7d2ec6679744ccc8dc
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-19 17:27:20 +0500

    string operations etc

--- hw.py
+++ hw.py
@@ -1,9 +1,9 @@
-x = "awesome"
-
-def myfunc():
-  global x
-  x = "fantastic"
-
-myfunc()
-
-print("Python is " + x)
+x = "spider man"
+a = x[2:5]
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+print(x.capitalize())
+print(x.lower())
+print(x.upper())
+print(a)
