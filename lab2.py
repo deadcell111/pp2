b84f8bab7253b029172772b2822f20d8ba7f1361
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-24 16:41:35 +0500

    Python - Join Two Lists

--- lab2.py
+++ lab2.py
@@ -1,20 +1,28 @@
 """Example
-Make a copy of a list with the copy() method:"""
+Join two list:"""
 
-thislist = ["apple", "banana", "cherry"]
-mylist = thislist.copy()
-print(mylist)
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
 
-"""Example
-Make a copy of a list with the list() method:"""
-
-thislist = ["apple", "banana", "cherry"]
-mylist = list(thislist)
-print(mylist)
+list3 = list1 + list2
+print(list3)
 
 """Example
-Make a copy of a list with the : operator:"""
+Append list2 into list1:"""
 
-thislist = ["apple", "banana", "cherry"]
-mylist = thislist[:]
-print(mylist)
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+
+"""Example
+Use the extend() method to add list2 at the end of list1:"""
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
