2acc670212512715e687bf41ea140e29bcdd38cd
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-24 16:34:10 +0500

    Python - Sort Lists

--- lab2.py
+++ lab2.py
@@ -1,51 +1,58 @@
-#Example
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-newlist = []
+"""Example
+Sort the list alphabetically:"""
 
-for x in fruits:
-  if "a" in x:
-    newlist.append(x)
-
-print(newlist)
-
-#Example
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-
-newlist = [x for x in fruits if "a" in x]
-
-print(newlist)
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
 
 """Example
-Only accept items that are not "apple":"""
+Sort the list numerically:"""
 
-newlist = [x for x in fruits if x != "apple"]
-
-"""Example
-With no if statement:"""
-
-newlist = [x for x in fruits]
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
 
 """Example
-You can use the range() function to create an iterable:"""
+Sort the list descending:"""
 
-newlist = [x for x in range(10)]
-
-"""Example
-Accept only numbers lower than 5:"""
-
-newlist = [x for x in range(10) if x < 5]
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
 
 """Example
-Set the values in the new list to upper case:"""
+Sort the list descending:"""
 
-newlist = [x.upper() for x in fruits]
-
-"""Example
-Set all values in the new list to 'hello':"""
-
-newlist = ['hello' for x in fruits]
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
 
 """Example
-Return "orange" instead of "banana":"""
+Sort the list based on how close the number is to 50:"""
 
-newlist = [x if x != "banana" else "orange" for x in fruits]
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
+"""Example
+Case sensitive sorting can give an unexpected result:"""
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist)
+
+"""Example
+Perform a case-insensitive sort of the list:"""
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+
+"""Example
+Reverse the order of the list items:"""
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
