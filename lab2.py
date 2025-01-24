f5d7f6ddee6d5acb0af289cc8320575971907270
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-24 17:36:26 +0500

    Python Sets

--- lab2.py
+++ lab2.py
@@ -1,16 +1,57 @@
 #Example
-#Join two tuples:
+#Create a Set:
 
-tuple1 = ("a", "b" , "c")
-tuple2 = (1, 2, 3)
-
-tuple3 = tuple1 + tuple2
-print(tuple3)
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
 
 #Example
-#Multiply the fruits tuple by 2:
+#Duplicate values will be ignored:
 
-fruits = ("apple", "banana", "cherry")
-mytuple = fruits * 2
+thisset = {"apple", "banana", "cherry", "apple"}
 
-print(mytuple)
+print(thisset)
+
+#Example
+#True and 1 is considered the same value:
+
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+print(thisset)
+
+#Example
+#False and 0 is considered the same value:
+
+thisset = {"apple", "banana", "cherry", False, True, 0}
+
+print(thisset)
+
+#Example
+#Get the number of items in a set:
+
+thisset = {"apple", "banana", "cherry"}
+
+print(len(thisset))
+
+#Example
+#String, int and boolean data types:
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+#Example
+#A set with strings, integers and boolean values:
+
+set1 = {"abc", 34, True, 40, "male"}
+
+#Example
+#What is the data type of a set?
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+#Example
+#Using the set() constructor to make a set:
+
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
