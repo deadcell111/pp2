63a6aacb823b4902446f1dc5aca25d0431c54b9c
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-24 18:05:12 +0500

    Join Sets

--- lab2.py
+++ lab2.py
@@ -1,6 +1,35 @@
-#Loop through the set, and print the values:
+#Join set1 and set2 into a new set:
 
-thisset = {"apple", "banana", "cherry"}
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
 
-for x in thisset:
-  print(x)
+set3 = set1.union(set2)
+print(set3)
+
+#Join multiple sets with the union() method:
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+
+myset = set1.union(set2, set3, set4)
+print(myset)
+
+#Use | to join two sets:
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+
+myset = set1 | set2 | set3 |set4
+print(myset)
+
+#Join a set with a tuple:
+
+x = {"a", "b", "c"}
+y = (1, 2, 3)
+
+z = x.union(y)
+print(z)
