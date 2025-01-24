2540e9a3350c442dae4577ae694c2062b3235acd
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-24 17:38:25 +0500

    Access Set Items

--- lab2.py
+++ lab2.py
@@ -1,57 +1,21 @@
 #Example
-#Create a Set:
-
-thisset = {"apple", "banana", "cherry"}
-print(thisset)
-
-#Example
-#Duplicate values will be ignored:
-
-thisset = {"apple", "banana", "cherry", "apple"}
-
-print(thisset)
-
-#Example
-#True and 1 is considered the same value:
-
-thisset = {"apple", "banana", "cherry", True, 1, 2}
-
-print(thisset)
-
-#Example
-#False and 0 is considered the same value:
-
-thisset = {"apple", "banana", "cherry", False, True, 0}
-
-print(thisset)
-
-#Example
-#Get the number of items in a set:
+#Loop through the set, and print the values:
 
 thisset = {"apple", "banana", "cherry"}
 
-print(len(thisset))
+for x in thisset:
+  print(x)
 
 #Example
-#String, int and boolean data types:
+#Check if "banana" is present in the set:
 
-set1 = {"apple", "banana", "cherry"}
-set2 = {1, 5, 7, 9, 3}
-set3 = {True, False, False}
+thisset = {"apple", "banana", "cherry"}
 
-#Example
-#A set with strings, integers and boolean values:
-
-set1 = {"abc", 34, True, 40, "male"}
+print("banana" in thisset)
 
 #Example
-#What is the data type of a set?
+#Check if "banana" is NOT present in the set:
 
-myset = {"apple", "banana", "cherry"}
-print(type(myset))
+thisset = {"apple", "banana", "cherry"}
 
-#Example
-#Using the set() constructor to make a set:
-
-thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
-print(thisset)
+print("banana" not in thisset)
