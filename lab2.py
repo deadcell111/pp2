47204f09ffb82de50510f8d3dcbec1f7a8c0ce6d
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-24 17:20:36 +0500

    Python - Update Tuples

--- lab2.py
+++ lab2.py
@@ -1,42 +1,41 @@
 """Example
-Print the second item in the tuple:"""
+Convert the tuple into a list to be able to change it:"""
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+"""Example
+Convert the tuple into a list, add "orange", and convert it back into a tuple:"""
 
 thistuple = ("apple", "banana", "cherry")
-print(thistuple[1])
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
 
 """Example
-Print the last item of the tuple:"""
+Create a new tuple with the value "orange", and add that tuple:"""
 
 thistuple = ("apple", "banana", "cherry")
-print(thistuple[-1])
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
 
 """Example
-Return the third, fourth, and fifth item:"""
-
-thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
-print(thistuple[2:5])
-
-"""Example
-This example returns the items from the beginning to, but NOT included, "kiwi":"""
-
-thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
-print(thistuple[:4])
-
-"""Example
-This example returns the items from "cherry" and to the end:"""
-
-thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
-print(thistuple[2:])
-
-"""Example
-This example returns the items from index -4 (included) to index -1 (excluded)"""
-
-thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
-print(thistuple[-4:-1])
-
-"""Example
-Check if "apple" is present in the tuple:"""
+Convert the tuple into a list, remove "apple", and convert it back into a tuple:"""
 
 thistuple = ("apple", "banana", "cherry")
-if "apple" in thistuple:
-  print("Yes, 'apple' is in the fruits tuple")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+"""Example
+The del keyword can delete the tuple completely:"""
+
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple) #this will raise an error because the tuple no longer exists
