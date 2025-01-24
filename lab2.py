92b4864acff493b915fc050f83de4199bb5656f0
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-24 16:05:17 +0500

    Python Change List items

--- lab2.py
+++ lab2.py
@@ -1,42 +1,34 @@
 """Example
-Print the second item of the list:"""
+Change the second item:"""
 
 thislist = ["apple", "banana", "cherry"]
-print(thislist[1])
+thislist[1] = "blackcurrant"
+print(thislist)
 
 """Example
-Print the last item of the list:"""
+Change the values "banana" and "cherry" with the values "blackcurrant" and "watermelon":"""
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+"""Example
+Change the second value by replacing it with two new values:"""
 
 thislist = ["apple", "banana", "cherry"]
-print(thislist[-1])
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
 
 """Example
-Return the third, fourth, and fifth item:"""
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[2:5])
-
-"""Example
-This example returns the items from the beginning to, but NOT including, "kiwi":"""
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[:4])
-
-"""Example
-This example returns the items from "cherry" to the end:"""
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[2:])
-
-"""Example
-This example returns the items from "orange" (-4) to, but NOT including "mango" (-1):"""
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[-4:-1])
-
-"""Example
-Check if "apple" is present in the list:"""
+Change the second and third value by replacing it with one value:"""
 
 thislist = ["apple", "banana", "cherry"]
-if "apple" in thislist:
-  print("Yes, 'apple' is in the fruits list")
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+"""Example
+Insert "watermelon" as the third item:"""
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
