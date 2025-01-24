fccaa9b52fd96567e150dc023aaed70ca6048e1d
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-24 16:36:29 +0500

    Python - Copy Lists

--- lab2.py
+++ lab2.py
@@ -1,58 +1,20 @@
 """Example
-Sort the list alphabetically:"""
+Make a copy of a list with the copy() method:"""
 
-thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
-thislist.sort()
-print(thislist)
-
-"""Example
-Sort the list numerically:"""
-
-thislist = [100, 50, 65, 82, 23]
-thislist.sort()
-print(thislist)
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
 
 """Example
-Sort the list descending:"""
+Make a copy of a list with the list() method:"""
 
-thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
-thislist.sort(reverse = True)
-print(thislist)
-
-"""Example
-Sort the list descending:"""
-
-thislist = [100, 50, 65, 82, 23]
-thislist.sort(reverse = True)
-print(thislist)
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
 
 """Example
-Sort the list based on how close the number is to 50:"""
+Make a copy of a list with the : operator:"""
 
-def myfunc(n):
-  return abs(n - 50)
-
-thislist = [100, 50, 65, 82, 23]
-thislist.sort(key = myfunc)
-print(thislist)
-
-"""Example
-Case sensitive sorting can give an unexpected result:"""
-
-thislist = ["banana", "Orange", "Kiwi", "cherry"]
-thislist.sort()
-print(thislist)
-
-"""Example
-Perform a case-insensitive sort of the list:"""
-
-thislist = ["banana", "Orange", "Kiwi", "cherry"]
-thislist.sort(key = str.lower)
-print(thislist)
-
-"""Example
-Reverse the order of the list items:"""
-
-thislist = ["banana", "Orange", "Kiwi", "cherry"]
-thislist.reverse()
-print(thislist)
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
