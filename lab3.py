381f7b810bc3e5457e91b1cfc8424d0276274b3c
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-29 13:39:11 +0500

    Sixth Task

--- lab3.py
+++ lab3.py
@@ -1,32 +1,10 @@
-class BankAccount:
-    print("enter your name and account: ")
-    def __init__(self,owner = None,balance = None):
-        if owner is None and balance is None:
-            owner = input()
-            balance = int(input())
-        self.owner = owner
-        self.balance = balance
-
-    def deposit(self,money = None):
-        print("specify how much you want to add: ")
-        if money is None:
-            money = int(input())
-        self.balance += money
-        print("The new balance is: ",self.balance)
-    def withdraw(self,money = None):
-        print("specify how much you want to take: ")
-        if money is None:
-            money = int(input())
-        if self.balance >= money:
-            self.balance -= money
-            print("The new balance is: ",self.balance)
-        else:
-            print("insufficient funds on the card")
-if __name__ == "__main__":
-    bank = BankAccount()
-    bank.deposit()
-    bank.withdraw()
-
-        
-
-        
+l = []
+a = int(input("add size of list: "))
+for i in range(a):
+    b = int(input())
+    l.append(b)
+isp = lambda x: x > 1 and all(x % i != 0 for i in range(2,(x // 2)+1))
+primes = list(filter(isp,l))
+print("Prime numbers from list: ")
+for i in primes:
+    print(i)
