2b093460e81403cfa5817049b4f7cc6c0711a343
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-29 11:37:12 +0500

    Second Task

--- lab3.py
+++ lab3.py
@@ -1,12 +1,15 @@
-class Strings:
-    def __init__(self):
-        self.text = ""
-    def getString(self):
-        self.text = input()
-    def printString(self):
-        print(self.text.upper())
-#Example:
+class Shape:
+    def area(self):
+        print(0)
+class Square(Shape):
+    def __init__(self,length):
+        length = int(input())
+        self.length = length
+    def area(self):
+        print(pow(self.length,2))
+#Example:   
 if __name__ == "__main__":
-    obj = Strings()
-    obj.getString()
-    obj.printString()
+    obj = Shape()
+    obj.area()
+    square = Square(5)
+    square.area()
