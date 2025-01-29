27a7f5a690c0f01aec77cca8488794c76d38eb25
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-29 12:48:15 +0500

    Fifth Task

--- lab3.py
+++ lab3.py
@@ -1,36 +1,32 @@
-import math
+class BankAccount:
+    print("enter your name and account: ")
+    def __init__(self,owner = None,balance = None):
+        if owner is None and balance is None:
+            owner = input()
+            balance = int(input())
+        self.owner = owner
+        self.balance = balance
 
-class Point:    
-    def __init__(self, x = None, y = None):
-        if x is None and y is None:
-            x = int(input())
-            y = int(input())
-        self.x = x
-        self.y = y
-
-    def show(self):
-        print(self.x, " ", self.y)
-        
-    def move(self, nx = None, ny = None):
-        if nx is None and ny is None:
-            nx = int(input())
-            ny = int(input())
-        
-        self.x = nx
-        self.y = ny
-        
-    def dist(self, d):
-        print(math.sqrt((self.x - d.x) ** 2 + (self.y - d.y) ** 2))
-#Example:
+    def deposit(self,money = None):
+        print("specify how much you want to add: ")
+        if money is None:
+            money = int(input())
+        self.balance += money
+        print("The new balance is: ",self.balance)
+    def withdraw(self,money = None):
+        print("specify how much you want to take: ")
+        if money is None:
+            money = int(input())
+        if self.balance >= money:
+            self.balance -= money
+            print("The new balance is: ",self.balance)
+        else:
+            print("insufficient funds on the card")
 if __name__ == "__main__":
-    p1 = Point()
-    p2 = Point()
-    print("First point is: ")
-    p1.show()
-    print("Second point is: ")
-    p2.show()
-    p1.move()
-    print("First changed point: ")
-    p1.show()
-    print("The distant between 2 points: ")
-    p1.dist(p2)
+    bank = BankAccount()
+    bank.deposit()
+    bank.withdraw()
+
+        
+
+        
