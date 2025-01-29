ea95f4d9d4f582eb4e6aebe62094fe244ddee096
Talgat <kozhakhmrtov2006@gmai.com>
2025-01-29 13:43:00 +0500

    Function 1

--- lab3.py
+++ lab3.py
@@ -1,10 +1,4 @@
-l = []
-a = int(input("add size of list: "))
-for i in range(a):
-    b = int(input())
-    l.append(b)
-isp = lambda x: x > 1 and all(x % i != 0 for i in range(2,(x // 2)+1))
-primes = list(filter(isp,l))
-print("Prime numbers from list: ")
-for i in primes:
-    print(i)
+def Gr_into_oun(x):
+    return x * 28.3495231
+a = int(input("grams: "))
+print("grams in ounces: ", Gr_into_oun(a))
