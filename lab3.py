44308930a38c1b9d0dddd6cf9dfafb65e1d8d5a8
Talgat <kozhakhmrtov2006@gmai.com>
2025-02-02 14:24:08 +0500

    Function 5

--- lab3.py
+++ lab3.py
@@ -1,10 +1,7 @@
-l = [1, 2, 3, 4, 5]
-def filter_prime(x):
-    if x > 1:
-        for i in range (2, (x//2) + 1):
-            if x % i == 0:
-                return False
-        return True
-for i in range(len(l)):
-    if(filter_prime(l[i])):
-        print(l[i])
+from itertools import permutations
+def perm(s):
+    pl = [''.join(i) for i in permutations(s)]
+    for i in pl:
+        print(i)
+j = input()
+perm(j)
