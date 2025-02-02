f0bfa068a7244d0b4175327de7ba331e20f14ff9
Talgat <kozhakhmrtov2006@gmai.com>
2025-02-02 15:03:19 +0500

    Function 9

--- lab3.py
+++ lab3.py
@@ -1,12 +1,5 @@
-def spy_game(nums):
-    code = [0,0,7]
-    index = 0
-    for i in nums:
-        if i == code[index]:
-            index += 1
-        if len(code) == index:
-            return True
-    return False
-print(spy_game([1,2,4,0,0,7,5])) #True
-print(spy_game([1,0,2,4,0,5,7])) #True
-print(spy_game([1,7,2,0,4,5,0])) #False         
+import math
+def volume_of_sphere(r):
+    return 4/3 * math.pi * (r **3)
+r = int(input())
+print(volume_of_sphere(r))
