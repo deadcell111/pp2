43be44748ce65d56c11f51b669f19be9d1f7879a
Talgat <kozhakhmrtov2006@gmai.com>
2025-02-02 14:00:36 +0500

    Function 3

--- lab3.py
+++ lab3.py
@@ -1,5 +1,16 @@
-def Centigrade(F):
-    C = (5/9) * (F - 32)
-    return C
-F = int(input("Farenheit: "))
-print("In Centigrade: ",Centigrade(F))   
+def solve(numheads,numlegs):
+    """4x + 2y = 94
+    x + y = 35, x = 35 - y
+    140 - 2y = 94
+    -2y = -46 
+    y = 23
+    x = 12"""
+    for chick in range(numheads + 1):
+        rab = numheads - chick
+        if(chick * 2 + rab * 4) == numlegs:
+            return chick, rab
+heads = 35
+legs = 94
+print(solve(heads,legs))
+
+    
