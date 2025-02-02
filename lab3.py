7de29a830605617e9895b8e5bcbb2f64a83ca5e4
Talgat <kozhakhmrtov2006@gmai.com>
2025-02-02 16:31:48 +0500

    Function 13

--- lab3.py
+++ lab3.py
@@ -1,4 +1,17 @@
-def histogram(nums):
-    for num in nums:
-        print('*' * num)
-histogram([4, 9, 7])
+import random
+print("Hello! What is your name?")
+name = input()
+print(f"Well, {name}, I am thinking of a number between 1 and 20.\nTake a guess.")
+x = random.randint(1,20)
+rep = 0
+t = True
+while t:
+        y = int(input())
+        rep += 1
+        if x != y:
+            print("Your guess is too low.\nTake a guess.")
+            t = True
+        else:
+            print(f"Good job, {name}! You guessed my number in {rep} guesses!")
+            t = False
+
